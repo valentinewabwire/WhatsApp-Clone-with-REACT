d6587985aeae7c49d407101d57e9b1fef175b8f6
valentinewabwire <valentine.wabwire@tracom.co.ke>
2021-10-04 20:57:37 +0300

    license update

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAoJucabnTb87dnRicUvB4w3lAnZepruKU",
+    authDomain: "whats-app-web-clone-6457d.firebaseapp.com",
+    projectId: "whats-app-web-clone-6457d",
+    storageBucket: "whats-app-web-clone-6457d.appspot.com",
+    messagingSenderId: "843233359362",
+    appId: "1:843233359362:web:1e685a94c81f965735991b",
+    measurementId: "G-M7XRCEQS24"
+  };
